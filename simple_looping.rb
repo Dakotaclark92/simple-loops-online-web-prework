2e17d777c212a84af5ee9e8e44770e5f2a9fe53e
Dakota Clark <bannaray95@gmail.com>
2018-10-17 16:13:08 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,7 +34,7 @@ def until_iterator(7)
 end
 
 def for_iterator(7)
-  
+  for
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
